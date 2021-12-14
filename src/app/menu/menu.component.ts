70c687d1524143f2a2e4a418715641e78008ccb0
IsuruUDSilva <isuruudsilva@gmail.com>
2021-12-14 22:31:58 +0530

    add service for dependency injection

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+// import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 // const DISHES: Dish[] = [
 //   {
@@ -53,12 +54,13 @@ import { DISHES } from '../shared/dishes';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = DISHES;
+  dishes: Dish[] | undefined ;
 
   selectedDish:any
   
 
   ngOnInit(): void {
+    this.dishes = this.dishService.getDishes();
   }
 
   onSelect(dish: Dish) {
@@ -67,7 +69,7 @@ export class MenuComponent implements OnInit {
     console.log(this.selectedDish)
   }
 
-  constructor() { 
+  constructor(private dishService: DishService) { 
 
   }
 
